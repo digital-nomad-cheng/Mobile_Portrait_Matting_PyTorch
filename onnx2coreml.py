0e00d35cd5c9a45ed29bbabc80d8985a7d2dee34
vincent929 <vincentcheng929@gmail.com>
2019-08-30 10:44:33 +0800

    new version model

---
+++ onnx2coreml.py
@@ -0,0 +1,15 @@
+import onnx
+import coremltools
+from onnx_coreml import convert
+from coremltools.models import MLModel
+
+onnx_model = onnx.load('models/SegNet_portrait_epoch-0099_sim.onnx')
+onnx.checker.check_model(onnx_model)
+coreml_model = convert(onnx_model,
+    image_input_names={'input.1'},
+    preprocessing_args={"image_scale":1/255.})
+spec = coreml_model.get_spec()
+coremltools.utils.rename_feature(spec, 'input.1', 'input_1')
+spec.neuralNetwork.preprocessing[0].featureName='input_1'
+coreml_model = MLModel(spec)
+coreml_model.save('SegNet.mlmodel')
